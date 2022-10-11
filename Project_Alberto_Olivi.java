934860d70deaa0cdd9916c8d16288c9224e5b273
aolivi00 <aolivi00@email.cpcc.edu>
2022-10-10 20:46:57 -0400

    Update Project_Alberto_Olivi.java
    Modified to read input from a file
    
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_Alberto_Olivi.java
+++ Project_Alberto_Olivi.java
@@ -2,68 +2,90 @@
    AlbertoProject class Demo
 */
 
-import java.util.Scanner; //for scanner object
+import java.util.*; 
+import java.io.*;
 
 public class Project_Alberto_Olivi
 {
    public static void main(String[] args)
    {
-      String providerName, policyFirstName, policyLastName, smoking; 
-      int policyNumber = 0;
-      int policyAge = 0; 
-      double policyHeight = 0;
-      double policyWeight = 0; 
+      try
+      {
+         File file = new File("PolicyInformation.txt");
+         
+         Scanner inputFile = new Scanner(file);
+         
+         String providerName = "", policyFirstName = "", policyLastName = "", smoking = "", lineData = "";
+         int policyNumber = 0, policyAge = 0, smokers = 0, nonSmokers = 0;
+         double policyHeight = 0.0, policyWeight = 0.0;
+         
+         ArrayList<Policy> policy = new ArrayList<Policy>();
+         
+         while(inputFile.hasNext())       
+         {
+            lineData = inputFile.nextLine();
+            policyNumber = Integer.parseInt(lineData);
+            
+            providerName = inputFile.nextLine();
+            
+            policyFirstName = inputFile.nextLine();
+            
+            policyLastName = inputFile.nextLine();
+            
+            lineData = inputFile.nextLine();
+            policyAge = Integer.parseInt(lineData);
+            
+            smoking = inputFile.nextLine();
+            
+            lineData = inputFile.nextLine();
+            policyHeight = Double.parseDouble(lineData);
+            
+            lineData = inputFile.nextLine();
+            policyWeight = Double.parseDouble(lineData);
+            
+            if(inputFile.hasNext())
+            { 
+               inputFile.nextLine();
+            }
+            
+            Policy newpolicy = new Policy(policyNumber, providerName, policyFirstName, policyLastName, policyAge, smoking, policyHeight, policyWeight);
+            
+            policy.add(newpolicy);
+         }
+         
+         inputFile.close();
+         
+         for (int i = 0; i < policy.size(); i++)
+         {
+            System.out.printf("\nPolicy Number: " + policy.get(i).getNumber());
+            System.out.printf("\nProvider Name: " + policy.get(i).getName());
+            System.out.printf("\nPolicyholder's First Name: " + policy.get(i).getFirstName());
+            System.out.printf("\nPolicyholder's Last Name: " + policy.get(i).getLastName());
+            System.out.printf("\nPolicyholder's Age: " + policy.get(i).getAge());
+            System.out.printf("\nPolicyholder's Smoking Status (smoker/non-smoker): " + policy.get(i).getSmoker());
+            System.out.printf("\nPolicyholder's Height: " + policy.get(i).getHeight() + " inches");
+            System.out.printf("\nPolicyholder's Weight: " + policy.get(i).getWeight() + " pounds");
+            System.out.printf("\nPolicyholder's BMI: %,.2f", policy.get(i).getBMI());
+            System.out.printf("\nPolicy Price: $%,.2f", policy.get(i).getPolicyPrice());
+            System.out.println();
+            
+            if ((policy.get(i).getSmoker()).equals("smoker"))
+            {
+               smokers++;
+            }
+            else
+            {
+               nonSmokers++;
+            }
+         }
+         
+         System.out.println("\nThe number of policies with a smoker is: " + smokers);
+         System.out.println("The number of policies with a non-smoker is: " + nonSmokers);
+      }
       
-      //scanner object to store input
-      Scanner keyboard = new Scanner(System.in);      
-      
-      System.out.print("Please enter the Policy Number: ");
-      policyNumber = keyboard.nextInt();
-      keyboard.nextLine();
-      //
-      System.out.print("Please enter the Provider Name: ");
-      providerName = keyboard.nextLine();
-      
-      //
-      System.out.print("Please enter the Policyholder's First Name: ");
-      policyFirstName = keyboard.nextLine();
-      
-      //
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      policyLastName = keyboard.nextLine();
-      
-      //
-      System.out.print("Please enter the Policyholder's Age: ");
-      policyAge = keyboard.nextInt();
-      keyboard.nextLine();
-      //
-      System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      smoking = keyboard.nextLine();
-      
-      //
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      policyHeight = keyboard.nextDouble();
-      
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      policyWeight = keyboard.nextDouble();
-      
-      //object to store stock info
-      Policy policy1 = new Policy(policyNumber, providerName, policyFirstName, policyLastName, policyAge, smoking, policyHeight, policyWeight);
-
-      
-      //print-out
-      System.out.println();
-      
-      System.out.printf("Policy Number: " + policy1.getNumber());
-      System.out.printf("\nProvider Name: " + policy1.getName());
-      System.out.printf("\nPolicyholder’s First Name: " + policy1.getFirstName());
-      System.out.printf("\nPolicyholder’s Last Name: " + policy1.getLastName());
-      System.out.printf("\nPolicyholder’s Age: " + policy1.getAge());
-      System.out.printf("\nPolicyholder’s Smoking Status: " + smoking);
-      System.out.printf("\nPolicyholder’s Height: " + policy1.getHeight() + " inches");
-      System.out.printf("\nPolicyholder’s Weight: " + policy1.getWeight() + " pounds");
-      System.out.printf("\nPolicyholder’s BMI: %,.2f", policy1.getBMI());
-      System.out.printf("\nPolicy Price: $%,.2f", policy1.getPolicyPrice());
-                         
+      catch(IOException ex)
+      {
+         System.out.println("Something went wrong reading the file: " + ex.getMessage());
+      }                   
    }
 }
